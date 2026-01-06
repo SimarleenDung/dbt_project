758fa4118e6e94031ad83227e61b7f82694d2adc
SimarleenDung <simudung@gmail.com>
2026-01-06 16:12:18 +0530

    Revert "Added files in models folder"
    This reverts commit 08d3ba457e624ebf5d5d887af57f2cdd0cd5537e.

--- models/example/models/staging/stg_uc_one_customer_data.sql
+++
@@ -1,8 +0,0 @@
-select
-    customer_id,
-    first_name,
-    middle_name,
-    last_name,
-    city,
-    updated_at
-from {{ source('staging_source', 'dbt_uc_one_customer_data') }}
--- models/example/models/staging/stg_uc_one_sample_data.sql
+++
@@ -1,6 +0,0 @@
-select
-    lower(email) as email,
-    first_name,
-    middle_name,
-    last_name
-from {{ ref('uc_one_sample_data') }}
