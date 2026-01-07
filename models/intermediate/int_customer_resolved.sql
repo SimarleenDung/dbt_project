fba83c6e2609cd88a87e3f8058d5b725581f1a73
Simarleen Kaur Dung <simudung@gmail.com>
2026-01-07 11:17:26 +0000

    Converted int_customer_resolved to incremental model

--- models/intermediate/int_customer_resolved.sql
+++ models/intermediate/int_customer_resolved.sql
@@ -1,13 +1,39 @@
--- int_customer_resolved.sql
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'email',
+    on_schema_change = 'sync_all_columns'
+) }}
+
+with ranked as (
+
+    select
+        email,
+        first_name,
+        last_name,
+        city,
+        phone_number,
+        ingestion_ts,
+        row_number() over (
+            partition by email
+            order by ingestion_ts desc
+        ) as rn
+    from {{ ref('stg_customers') }}
+
+    {% if is_incremental() %}
+      where ingestion_ts > (
+        select coalesce(max(resolved_ts), '1900-01-01')
+        from {{ this }}
+      )
+    {% endif %}
+
+)
+
 select
     email,
-
-    max_by(first_name, ingestion_ts) as first_name,
-    max_by(last_name, ingestion_ts)  as last_name,
-    max_by(city, ingestion_ts)       as city,
-    max_by(phone_number, ingestion_ts) as phone_number,
-
-    max(ingestion_ts) as resolved_ts
-
-from {{ ref('stg_customers') }}
-group by email
+    first_name,
+    last_name,
+    city,
+    phone_number,
+    ingestion_ts as resolved_ts
+from ranked
+where rn = 1
